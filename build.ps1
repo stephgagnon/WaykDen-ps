a29458afbf7bd8ba8c629503c7f18b3eab427e80
Johann Dufaud <jdufaud@devolutions.net>
2019-10-11 13:59:13 -0400

    Fixed build

--- build.ps1
+++ build.ps1
@@ -2,8 +2,10 @@
 $ModuleName = 'WaykDen'
 Remove-Item -Path .\$ModuleName\package -Recurse -Force -ErrorAction SilentlyContinue
 
-& dotnet publish -f netcoreapp2.2 -c Release -o ./$ModuleName/package/Core ./src/WaykDen.sln
+& dotnet publish -f netcoreapp2.2 -c Release -o ./$ModuleName/package/$ModuleName/Core ./src/WaykDen.sln
 
 if (($PSVersionTable.Keys -contains "PSEdition") -and ($PSVersionTable.PSEdition -eq 'Desktop')) {
-	& dotnet publish -f net472 -c Release -o ./$ModuleName/package/Desktop ./src/WaykDen.sln
+	& dotnet publish -f net472 -c Release -o ./$ModuleName/package/$ModuleName/Desktop ./src/WaykDen.sln
 }
+
+Copy-Item .\src\$ModuleName.psd1 -Destination .\$ModuleName\package\$ModuleName
